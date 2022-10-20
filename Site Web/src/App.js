27a1caf36f78b6b256fc6c4255c40e0aa3c0d561
Noan07 <84435602+Noan07@users.noreply.github.com>
2022-10-20 10:16:53 +0200

    fix pb couleur / navbar

--- Site Web/src/App.js
+++ Site Web/src/App.js
@@ -7,6 +7,7 @@ import Home from './pages/Home';
 import Tendances from './pages/Tendances';
 import Decouvrir from './pages/Decouvrir';
 import Inscription from './pages/Inscription';
+import "./styles/index.scss"
 
 function App() {
   return (
