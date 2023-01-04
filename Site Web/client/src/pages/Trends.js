fdba7bea62706809133fa62e59fdcab165fdc6cf
bastien.ollier@etu.uca.fr <bastien.ollier@etu.uca.fr>
2023-01-04 15:53:58 +0100

    ajout titre a la page notification

--- Site Web/client/src/pages/Trends.js
+++ Site Web/client/src/pages/Trends.js
@@ -4,16 +4,16 @@ import Notif from '../components/Notif';
 
 const Trends = () => {
     return (
-        <>
+        <div id={'listeNotif'}>
             <Navbar />
+            <h1>Notification</h1>
             <Notif message="a like"/>
             <Notif message="a like"/>
             <Notif message="a like"/>
             <Notif message="a like"/>
             <Notif message="a like"/>
             <Notif message="a commenter votre poste"/>
-        </>
-    );
+        </div>);
 };
 
 export default Trends;
