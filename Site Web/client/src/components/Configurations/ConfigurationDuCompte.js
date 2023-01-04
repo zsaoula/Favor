2b1dbb1066ff053c6cd24b9cdf79efc47bf7f2e5
Darius BERTRAND <darius.bertrand@etu.uca.fr>
2023-01-04 15:15:46 +0100

    Mise à jour de 'Site Web/client/src/components/Configurations/ConfigurationDuCompte.js'

--- Site Web/client/src/components/Configurations/ConfigurationDuCompte.js
+++ Site Web/client/src/components/Configurations/ConfigurationDuCompte.js
@@ -3,23 +3,61 @@ import { NavLink } from "react-router-dom";
 
 
 
-const PolitiqueDeConfidentialite =()=>{
-    return(
-        <div className='PolitiqueDeConfidentialiteText'>
-           <p> Cette politique de confidentialité s’applique au site : Designed By GG.
-            La présente politique de confidentialité a pour but d’exposer aux utilisateurs du site :
+const ConfigurationDuCompte =()=>{
 
-            La manière dont sont collectées et traitées leurs données à caractère
-            personnel. Doivent être considérées comme données personnelles toutes les données étant susceptibles d’identifier un utilisateur. Il s’agit notamment du prénom et du nom, de l’âge, de l’adresse postale, l’adresse mail, la localisation de l’utilisateur ou encore son adresse IP ;
-            Quels sont les droits des utilisateurs concernant ces données ;
-            Qui est responsable du traitement des données à caractère personnes
-            collectées et traitées ;
-            A qui ces données sont transmises ;
-            Eventuellement, la politique du site en matière de fichiers “cookies”
-            </p>
-        </div>
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [controlPassword, setControlPassword] = useState('');
+
+    return(
+        <div>
+        <h2 className='ligneHorizontal'>Modifucation information du compte</h2>
+       
+            <div className="ModifucationInformationDuCompte">
+                        
+                        <div className='ModificationInformationDuCompteText'>
+                        <span>email</span>
+                        <span>mot de passe</span>
+                        <span>comfirmation mot de passe</span>
+                        </div>
+                        
+                        <div className='ModificationInformationDuCompteText'>
+                        
+                        <input  type="text" 
+                                name="email"
+                                id="email"
+                                onChange={(e) => setEmail(e.target.value)}
+                                value={email}/>
+                        <br />
+                        <div className='email error'></div>
+                        <input  type="password"
+                                name="password"
+                                id="password"
+                                onChange={(e) => setPassword(e.target.value)}
+                                value={password}/>
+                        <br/>
+                        <div className='password error'></div>
+                        <input  type="password" 
+                                name="password"
+                                id="password-conf"
+                                onChange={(e) => setControlPassword(e.target.value)}
+                                value={controlPassword}/>
+                                <div className='password error'></div>
+                        </div>
+            </div>
+        <div />
+        <h2 className='ligneHorizontal'>Suppression du compte</h2>
+        <div className='buttonSuppressionCompte'>
+
+        <button>
+            supprimer votre compte    
+        </button>
+        <span>gggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg</span>
+        </div> 
+    </div>
     )
 }
+
 
 
 
