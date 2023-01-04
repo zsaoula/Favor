5c55de21e69c4a29cc2afece4c1e8e83b35ec7a8
bastien.ollier@etu.uca.fr <bastien.ollier@etu.uca.fr>
2023-01-04 15:07:47 +0100

    notification

--- Site Web/client/src/pages/Trends.js
+++ Site Web/client/src/pages/Trends.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import Notif from '../components/Notif';
 
 const Trends = () => {
     return (
-        <Navbar />
+        <>
+            <Navbar />
+            <Notif message="a like"/>
+            <Notif message="a like"/>
+            <Notif message="a like"/>
+            <Notif message="a like"/>
+            <Notif message="a like"/>
+            <Notif message="a commenter votre poste"/>
+        </>
     );
 };
 
