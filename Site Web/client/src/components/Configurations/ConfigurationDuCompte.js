acf7cef19d962520562d25934940aad4b4eea6da
Darius BERTRAND <darius.bertrand@etu.uca.fr>
2023-01-04 15:10:55 +0100

    Mise à jour de 'Site Web/client/src/components/Configurations/ConfigurationDuCompte.js'

--- Site Web/client/src/components/Configurations/ConfigurationDuCompte.js
+++ Site Web/client/src/components/Configurations/ConfigurationDuCompte.js
@@ -3,26 +3,21 @@ import { NavLink } from "react-router-dom";
 
 
 
-const ConfigurationDuCompte =()=>{
+const PolitiqueDeConfidentialite =()=>{
     return(
-        <div>
-        <h2>Modifucation information du compte</h2>
-        <div className='ligneHorizontal'>
-            <div>
-                <span>adresse mail : mail</span>
-                <span>mot de passe  : mdp</span>
-                <span>confirmer le mot de passe  : mdp</span>
-            </div>
+        <div className='PolitiqueDeConfidentialiteText'>
+           <p> Cette politique de confidentialité s’applique au site : Designed By GG.
+            La présente politique de confidentialité a pour but d’exposer aux utilisateurs du site :
+
+            La manière dont sont collectées et traitées leurs données à caractère
+            personnel. Doivent être considérées comme données personnelles toutes les données étant susceptibles d’identifier un utilisateur. Il s’agit notamment du prénom et du nom, de l’âge, de l’adresse postale, l’adresse mail, la localisation de l’utilisateur ou encore son adresse IP ;
+            Quels sont les droits des utilisateurs concernant ces données ;
+            Qui est responsable du traitement des données à caractère personnes
+            collectées et traitées ;
+            A qui ces données sont transmises ;
+            Eventuellement, la politique du site en matière de fichiers “cookies”
+            </p>
         </div>
-        <h2>Suppression du compte</h2>
-        <div className='ligneHorizontal'/>
-        <div >
-        <button>
-            supprimer votre compte    
-        </button>
-        <span>gggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg</span>
-        </div> 
-    </div>
     )
 }
 
